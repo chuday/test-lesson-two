eadf6a731064cf55cbbb417c286c049e5c6a4021
chuday <mikhail_chudaew505@mail.ru>
2021-02-03 23:50:22 +0500

    lesson 1

---
+++ lesson one/lesson one/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson one
+//
+//  Created by Михаил Чудаев on 03.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
